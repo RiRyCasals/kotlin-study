e3a1d543e5c118f8e5fab694746e62e5e40a5204
RiRyCasals <rirycasals@gmail.com>
2022-04-27 12:29:00 +0900

    p84まで終了(ch3終了)

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.helloandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isTapToggleHead: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.tapHere.setOnClickListener {
+            if (isTapToggleHead) {
+                binding.textView.text = "ボタンがタップされました"
+                isTapToggleHead = false
+            } else {
+                binding.textView.text = "タップするたびに表示が切り替わります"
+                isTapToggleHead = true
+            }
+        }
     }
 }
